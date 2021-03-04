2304d061d5a6a882effc60ec0ce219d8710e4c29
Miguel Scatolin <miguel.scatolin@gmail.com>
2021-03-04 19:45:44 -0300

    FIT: insertion and merge sort

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,10 +15,75 @@ void populateBases(Base *bases, std::ifstream &file, int numberOfBases) {
 
             bases[i] = newBase;
         }
+}
 
-        for (int i = 0; i < numberOfBases; i++) {
-            std::cout << bases[i].name << " - " << bases[i].distance << std::endl;
+void insertionSort(Base *bases, int numberOfBases) {
+    int i,j;
+    Base aux;
+    for(i = 1; i < numberOfBases; i++) {
+        aux = bases[i];
+        j = i - 1;
+
+        while(j >= 0 && aux.distance > bases[j].distance) {
+            bases[j + 1] = bases[j];
+            j--;
         }
+
+        bases[j + 1] = aux;
+    }
+}
+
+Base *merge(Base leftList[], Base rightList[], int numberOfLeftElements, int numberOfRightElements) {
+    int i = 0, j = 0, k = 0;
+    Base* sortedMerge = new Base[numberOfRightElements + numberOfLeftElements];
+
+    while (i < numberOfLeftElements && j < numberOfRightElements) {
+        if (leftList[i].distance >= rightList[j].distance) {
+            sortedMerge[k] = leftList[i];
+            i++;
+        } else {
+            sortedMerge[k] = rightList[j];
+            j++;
+        }
+        k++;
+    }
+
+    if (k < numberOfLeftElements + numberOfRightElements) {
+        for(; i < numberOfLeftElements; i++) {
+            sortedMerge[k] = leftList[i];
+            k++;
+        }
+        for(; j < numberOfRightElements; j++) {
+            sortedMerge[k] = rightList[j];
+            k++;
+        }
+    }
+
+    return sortedMerge;
+}
+
+Base *splitAndMergeSorted(Base bases[], int leftIndex, int rightIndex) {
+    if(leftIndex < rightIndex) {
+        int middleIndex = (leftIndex + rightIndex) / 2;
+        Base *leftList = splitAndMergeSorted(bases, leftIndex, middleIndex);
+        Base *rightList = splitAndMergeSorted(bases, middleIndex + 1, rightIndex);
+        int numberOfLeftElements = middleIndex - leftIndex + 1;
+        int numberOfRightElements = rightIndex - middleIndex;
+        return merge(leftList, rightList, numberOfLeftElements, numberOfRightElements);
+    }
+    Base* singleElement = new Base[1];
+    singleElement[0] = bases[leftIndex];
+    return singleElement;
+}
+
+void mergeSort(Base bases[], int numberOfBases) {
+    bases = splitAndMergeSorted(bases, 0, numberOfBases - 1);
+}
+
+void printFirstBases(Base *bases) {
+    for (int i = 0; i < 7; i++) {
+        std::cout << bases[i].name << " - " << bases[i].distance << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -32,8 +97,13 @@ int main(int argc, char* argv[]) {
             throw  "Erro ao abrir arquivo de mapa";
 
         int numberOfBases = atoi(argv[2]);
+        if(numberOfBases < 7)
+            throw "Mínimo de 7 bases devem ser analisadas";
+
         Base* bases = new Base[numberOfBases];
         populateBases(bases, file, numberOfBases);
+        mergeSort(bases, numberOfBases);
+        printFirstBases(bases);
 
         return 0;
     }
