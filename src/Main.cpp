e79b7a3f66a738a25166d1493f929b93b0c48794
Miguel Scatolin <miguel.scatolin@gmail.com>
2021-03-06 17:58:51 -0300

    FIT: using median pivot on the non recursive quick sort

--- src/Main.cpp
+++ src/Main.cpp
@@ -91,7 +91,7 @@ void partition(Base bases[], int leftIndex, int rightIndex, int &i, int &j) {
     Base pivot, aux;
     i = leftIndex;
     j = rightIndex;
-    pivot = bases[(i + j)/2];
+    pivot = bases[0];
     do {
         while(pivot.distance < bases[i].distance) i++;
         while(pivot.distance > bases[j].distance) j--;
@@ -152,7 +152,38 @@ class Interval
         int right;
 };
 
-void nonRecursiveQuickSort(Base bases[], int numberOfBases) {
+Base getMedianPivot(Base bases[], int leftIndex, int rightIndex) {
+    Base first = bases[leftIndex],
+            last = bases[rightIndex],
+            middle = bases[(leftIndex + rightIndex)/2];
+    if((middle.distance >= last.distance && middle.distance <= first.distance) || (middle.distance <= last.distance && middle.distance >= first.distance))
+        return middle;
+    if((last.distance >= middle.distance && last.distance <= first.distance) || (last.distance <= middle.distance && last.distance >= first.distance))
+        return last;
+    else
+        return first;
+}
+
+void partitionWithMedianPivot(Base bases[], int leftIndex, int rightIndex, int &i, int &j) {
+    Base pivot, aux;
+    i = leftIndex;
+    j = rightIndex;
+    pivot = getMedianPivot(bases, leftIndex, rightIndex);
+
+    do {
+        while(pivot.distance < bases[i].distance) i++;
+        while(pivot.distance > bases[j].distance) j--;
+        if(i <= j) {
+            aux = bases[i];
+            bases[i] = bases[j];
+            bases[j] = aux;
+            i++;
+            j--;
+        }
+    } while(i <= j);
+}
+
+void nonRecursiveQuickSortWithMedianPivot(Base bases[], int numberOfBases) {
     Stack<Interval> stack = Stack<Interval>();
     int leftIndex = 0;
     int rightIndex = numberOfBases - 1;
@@ -161,7 +192,7 @@ void nonRecursiveQuickSort(Base bases[], int numberOfBases) {
     stack.Push(currentInterval);
     do {
         if (rightIndex > leftIndex) {
-            partition(bases, leftIndex, rightIndex, i, j);
+            partitionWithMedianPivot(bases, leftIndex, rightIndex, i, j);
             if ((j-leftIndex)>(rightIndex-i)) {
                 currentInterval.right = j;
                 currentInterval.left = leftIndex;
@@ -199,7 +230,7 @@ int main(int argc, char* argv[]) {
 
         Base* bases = new Base[numberOfBases];
         populateBases(bases, file, numberOfBases);
-        nonRecursiveQuickSort(bases, numberOfBases);
+        nonRecursiveQuickSortWithMedianPivot(bases, numberOfBases);
         printFirstBases(bases);
 
         return 0;
